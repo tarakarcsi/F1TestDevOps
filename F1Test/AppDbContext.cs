1deb15ec57092c2f180544b24850cda0a374c829
tarakarcsi <tarakarcsi94@gmail.com>
2020-11-12 13:10:51 +0100

    first

---
+++ F1Test/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace F1Test
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=MyDatabase.db");
+        }
+    }
+}
---
+++ F1Test/Models/Team.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace F1Test.Models
+{
+    public class Team
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Csapatnév")]
+        [Required(ErrorMessage = "A {0} kötelező mező!")]
+        public string Name { get; set; }
+
+        [Display(Name = "Alapítás éve")]
+        [Required(ErrorMessage = "A {0} kötelező mező!")]
+        public DateTime? FoundationYear { get; set; }
+
+        [Display(Name = "Világbajnoki címek")]
+        [Required(ErrorMessage = "A {0} kötelező mező!")]
+        public int Titles { get; set; } // number of championship wins
+
+        public bool HasPayed { get; set; } // entry fee payed or not
+    }
+}
